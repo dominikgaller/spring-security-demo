1b874cd96cfd2135f17bc02ce8049fa1f3d8b25d
Dominik Galler <dominik.galler@esentri.com>
2019-08-01 15:58:56 +0200

    Initialize controller and app

--- src/main/java/com/dominikgaller/spring/security/demo/SpringSecurityDemo.java
+++ src/main/java/com/dominikgaller/spring/security/demo/SpringSecurityDemo.java
@@ -1,4 +1,13 @@
 package com.dominikgaller.spring.security.demo;
 
+import com.apple.eawt.Application;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
 public class SpringSecurityDemo {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
 }
